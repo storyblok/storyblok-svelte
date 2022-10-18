63e297def7f4748890511d6f12669a1be070e07a
Alex Jover <alexjovermorales@gmail.com>
2022-10-18 12:10:00 +0100

    chore: fix the sveltekit playground

--- playground-sveltekit/src/routes/+layout.svelte
+++ playground-sveltekit/src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script>
-  import { useStoryblokBridge } from "@storyblok/svelte";
-  import { onMount } from "svelte";
+  import Feature from "../components/Feature.svelte";
+  import Grid from "../components/Grid.svelte";
+  import Page from "../components/Page.svelte";
+  import Teaser from "../components/Teaser.svelte";
+  import { apiPlugin, storyblokInit } from "@storyblok/svelte";
 
-  export let data;
-  let { story } = data;
-  onMount(() => {
-    useStoryblokBridge(story.id, (newStory) => (story = newStory));
+  storyblokInit({
+    accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
+    use: [apiPlugin],
+    components: {
+      feature: Feature,
+      grid: Grid,
+      page: Page,
+      teaser: Teaser,
+    },
   });
 </script>
 
