07d9587c5d6a77189c9a71d04da26b35114bae1f
Alex Jover <alexjovermorales@gmail.com>
2022-10-19 13:20:57 +0100

    chore: move storyblokInit ot layout.js

---
+++ playground-sveltekit/src/routes/+layout.js
@@ -0,0 +1,16 @@
+import Feature from "../components/Feature.svelte";
+import Grid from "../components/Grid.svelte";
+import Page from "../components/Page.svelte";
+import Teaser from "../components/Teaser.svelte";
+import { apiPlugin, storyblokInit } from "@storyblok/svelte";
+
+storyblokInit({
+  accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
+  use: [apiPlugin],
+  components: {
+    feature: Feature,
+    grid: Grid,
+    page: Page,
+    teaser: Teaser,
+  },
+});
--- playground-sveltekit/src/routes/+layout.svelte
+++ playground-sveltekit/src/routes/+layout.svelte
@@ -1,22 +1,3 @@
-<script>
-  import Feature from "../components/Feature.svelte";
-  import Grid from "../components/Grid.svelte";
-  import Page from "../components/Page.svelte";
-  import Teaser from "../components/Teaser.svelte";
-  import { apiPlugin, storyblokInit } from "@storyblok/svelte";
-
-  storyblokInit({
-    accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
-    use: [apiPlugin],
-    components: {
-      feature: Feature,
-      grid: Grid,
-      page: Page,
-      teaser: Teaser,
-    },
-  });
-</script>
-
 <main class="prose prose-xl">
   <slot />
 </main>
