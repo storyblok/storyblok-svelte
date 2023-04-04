c8193d6e25bc18966a68cd5c535796b43a2b73b3
Roberto Butti <roberto.butti@gmail.com>
2023-04-04 12:06:06 +0200

    chore: svelte playground, eliminating resolve

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [svelte(), basicSsl()],
-  resolve: {
-    alias: {
-      "@storyblok/svelte": path.resolve(
-        __dirname,
-        "../node_modules/@storyblok/svelte"
-      ),
-    },
-  },
 });
