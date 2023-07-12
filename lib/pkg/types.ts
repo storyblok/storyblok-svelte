4f1e70ab81f7c1d1e70fe733090d62268819f061
Alex Jover <alexjovermorales@gmail.com>
2023-07-12 12:22:37 +0200

    refactor: do the TS update and surpress the linter error

--- lib/pkg/types.ts
+++ lib/pkg/types.ts
@@ -2,6 +2,7 @@ import type { SbSDKOptions } from "@storyblok/js";
 import type { SvelteComponent } from "svelte";
 
 export interface SbSvelteComponentsMap {
+  // @ts-ignore
   [name: string]: typeof SvelteComponent<any>;
 }
 
