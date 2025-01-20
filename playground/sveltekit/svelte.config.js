797cae10a31d1ccfa6a3cd8752d0ab2946fcad7f
alvarosabu <alvaro.saburido@gmail.com>
2025-01-20 15:45:53 +0100

    chore(playground): add pathe package and update configuration for aliasing

--- playground/sveltekit/svelte.config.js
+++ playground/sveltekit/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'pathe';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +12,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
 };
 
 export default config;
--- playground/sveltekit/vite.config.ts
+++ playground/sveltekit/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { resolve } from 'pathe';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), basicSsl()],
+	resolve: {
+		alias: {
+		  '@storyblok/svelte': resolve(__dirname, '../../src/lib/index.ts'),
+		},
+	},
 });
