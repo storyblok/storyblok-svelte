cda8db21556902c426c0a931762cca46eee11701
edodusi <edo@edoardodusi.com>
2025-03-21 16:11:10 +0100

    chore: update deps and switch to ssl for playgrounds

--- playground/sveltekit/svelte.config.js
+++ playground/sveltekit/svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'pathe';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
--- playground/sveltekit/vite.config.ts
+++ playground/sveltekit/vite.config.ts
@@ -1,10 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import { resolve } from 'pathe';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()],
+	plugins: [sveltekit(), basicSsl({
+		name: 'my-ssl',
+		domains: ['localhost']
+	})] as PluginOption[],
 	resolve: {
 		alias: {
 		  '@storyblok/svelte': resolve(__dirname, '../../src/lib/index.ts'),
